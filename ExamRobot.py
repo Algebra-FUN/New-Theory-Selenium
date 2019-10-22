099c62cacdc39f303d5b8ac9f810f1caf1a98def
FLYFAN <oncwnuHv6d0vSjvoRm6Eps864IiY@git.weixin.qq.com>
2019-10-22 21:59:57 +0800

    [fix]auto-captcha-fill

--- ExamRobot.py
+++ ExamRobot.py
@@ -26,21 +26,12 @@ class ExamRobot:
             annotation = self.browser.find_element_by_css_selector(
                 'annotation')
             text = annotation.text
-            result = 0
-            if 'int' not in text:
-                captcha_box = self.browser.find_element_by_css_selector(
-                    '.captcha-container > div:first-child > div')
-                self.browser.execute_script(
-                    'arguments[0].click();', captcha_box)
-                self.fill_captcha()
-                return
             print('annotation', text)
+            result = 0
             re_sult = re.search(
-                r'\\int_{(\d+)}\^{(\d+)}\((\d+)x\^2\+(\d+)x\+(\d+)\)dx=\?', text)
-            a, b, k3, k2, k1 = [int(k) for k in re_sult.groups()]
-            k3, k2 = k3/3, k2/2
-            result = int(k3 * (b ** 3 - a ** 3) + k2 *
-                         (b ** 2 - a ** 2) + k1 * (b - a))
+                r'(\d+)\\times(\d+)\+(\d+)\-(\d+)=\?', text)
+            a, k1, k2, k3 = [int(k) for k in re_sult.groups()]
+            result = int(a * k1 + k2 - k3)
             print('result', result)
             inputer = self.browser.find_element_by_css_selector(
                 '.captcha-container > div:last-child > input')
@@ -79,7 +70,8 @@ class ExamRobot:
                         selected = ''.join(s_arr)
                         score += 2 if selected == keys else 0
                     print(text)
-                    print('selected:{},key:{},current-score:{}'.format(selected,keys,score))
+                    print(
+                        'selected:{},key:{},current-score:{}'.format(selected, keys, score))
                 else:
                     for ainput in inputs:
                         if ainput.get_attribute('value') in keys:
